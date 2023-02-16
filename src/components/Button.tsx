0da796bb48ad0de30d077b8b962130b4422527e2
sanderdsz <sanderdsz@gmail.com>
2023-02-16 16:26:43 -0300

    chore: minor index design change

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'preact/hooks'
-
 export default function Button() {
-    const [count, setCount] = useState(0)
 
-    return (
-        <div>
-            <div className="w-fit h-fit px-4 py-2 drop-shadow-primary bg-primary-300">
-                <span className="font-medium text-2xl text-snow-100">
-                    Lets <span className="font-extrabold italic">TALK!</span>
-                </span>
-            </div>
+  return (
+    <div>
+      <a href="https://linkedin.com/sandersz/" target="_blank">
+        <div className="h-fit w-fit bg-primary-300 px-4 py-2 drop-shadow-primary">
+          <span className="text-2xl font-medium text-snow-100">
+            Lets <span className="font-extrabold italic">TALK!</span>
+          </span>
         </div>
-    )
+      </a>
+    </div>
+  );
 }
