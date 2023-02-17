3f6e11213850369bc47343dbb3d4368f2d05cb02
sanderdsz <sanderdsz@gmail.com>
2023-02-16 21:44:35 -0300

    fix: button width and href

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ export default function Button() {
 
   return (
     <div>
-      <a href="https://linkedin.com/sandersz/" target="_blank">
+      <a href="https://linkedin.com/in/sandersz/" target="_blank">
         <div className="h-fit w-fit bg-primary-300 px-4 py-2 drop-shadow-primary">
           <span className="text-2xl font-medium text-snow-100">
             Lets <span className="font-extrabold italic">TALK!</span>
